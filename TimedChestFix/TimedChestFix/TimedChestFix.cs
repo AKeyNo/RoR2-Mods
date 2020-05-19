9d76733112825829242d51b6a469aee6b9f856e3
Ariel Aquino <arielaquino.cs@gmail.com>
2020-05-19 02:05:16 -0700

    added TimedChestFix

---
+++ TimedChestFix/TimedChestFix/TimedChestFix.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using RoR2;
+
+namespace AKeyNo
+{
+    [BepInDependency("com.bepis.r2api")]
+    [BepInPlugin("com.AKeyNo.lockedchesttimer", "Timed Chest Fix", "1.0.0")]
+    public class LockedFix : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            Chat.AddMessage("Loaded Timed Fix Change!");
+
+            On.RoR2.TimedChestController.FixedUpdate += (orig, self) =>
+            {
+                self.lockTime = 1200f;
+                orig(self);
+            };
+        }
+    }
+}
